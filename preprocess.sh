e6bffdd856c9591c5eb1ea3568ae8b25bddd8511
Hadia Hameed <hadiahameeduet@gmail.com>
2020-09-17 13:44:32 +0500

    shell script to preprocess data

---
+++ preprocess.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+dataset='ohio' #ohio or oaps
+if [ "$dataset" = "ohio" ]; then
+    root_directory="../../../../PHI/PHI_OHIO/" 
+    data_directory=$root_directory"data/"
+fi
+if [ "$dataset" = "oaps" ]; then
+    root_directory="../../../../PHI/PHI_OAPS/" 
+    data_directory=$root_directory"OpenAPS_data/n=88_OpenAPSDataAugust242018/" #../../../data/PHI/PHI_OAPS/OpenAPS_data/n=88_OpenAPSDataAugust242018/
+
+fi
+filter_data=True
+normalize_data=False
+threshold=15 #remove CGM readings below 15 mg/dL
+history_window=12 #no. of past values to use to make estimations of future values
+prediction_window=30 #no. of future values to predict (6 denotes a prediction horizon of 5 * 6 = 30 min)
+ablation_code=0 #0 = 'unfiltered_imputed', 1 = 'filtered_imputed', 2 = 'unfiltered_unimputed', 3 = 'filtered_unimputed', 4 = 'main folder'
+python $PWD/preprocess_$dataset.py $root_directory $data_directory $filter_data $normalize_data $threshold $history_window $prediction_window $ablation_code
